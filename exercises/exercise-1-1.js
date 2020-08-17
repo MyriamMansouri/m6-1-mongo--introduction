f181d69d30623904e44366a5a786dcfb97da892a
myriam mansouri <myriam.mansouri@gmail.com>
2020-08-17 12:29:48 -0400

    # Exercise 1.2 - Get Users (`.find`)

--- exercises/exercise-1-1.js
+++ exercises/exercise-1-1.js
@@ -3,12 +3,17 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
 const dbFunction = async (dbName) => {
-  const client = await MongoClient(MONGO_URI, { useUnifiedTopology: true });
+  const client = await MongoClient(MONGO_URI, options);
   await client.connect();
   const db = client.db(dbName);
   console.log("connected");
-  await db.collection("users").insertOne({name:"Buck Rogers"})
+  await db.collection("users").insertOne({name:"Minnie Rogers"})
   client.close();
   console.log("disconnected");
 };
